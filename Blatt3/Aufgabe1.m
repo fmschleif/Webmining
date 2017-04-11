854cd9f3607b5cf2d6f9c5af826fdb0ad33a4f66
Irame <irame94@gmail.com>
2017-04-11 20:02:04 +0200

    Added solution for sheet 3 exercise 1b)

--- Blatt3/Aufgabe1.m
+++ Blatt3/Aufgabe1.m
@@ -1,3 +1,4 @@
+clear;
 close all;
 load('iris_test_plain.mat');
 
@@ -26,3 +27,25 @@ for i = 1:size(combs3d,1)
     zlabel(sprintf('Dimension %d', combs3d(i,3)));
     title(sprintf('Dimensions %d & %d & %d', combs3d(i,1), combs3d(i,2), combs3d(i,3)));
 end
+
+%% b)
+irisDataNormMean = irisInputs - repmat(mean(irisInputs, 2), 1, size(irisInputs,2));
+
+irisDataNormStdDiv = irisDataNormMean ./ repmat(sqrt(mean(irisDataNormMean.^2, 2)), 1, size(irisInputs,2));
+
+figure('Name', 'Normed (Dimension 3 & 4)');
+subplot(1,3,1);
+scatter(irisInputs(3,:), irisInputs(4,:), 50, irisTargets');
+xlabel('Dimension 3');
+ylabel('Dimension 4');
+title('Original');
+subplot(1,3,2);
+scatter(irisDataNormMean(3,:), irisDataNormMean(4,:), 50, irisTargets');
+xlabel('Dimension 3');
+ylabel('Dimension 4');
+title('Normalized Mean');
+subplot(1,3,3);
+scatter(irisDataNormStdDiv(3,:), irisDataNormStdDiv(4,:), 50, irisTargets');
+xlabel('Dimension 3');
+ylabel('Dimension 4');
+title('Normalized StdDiv');
